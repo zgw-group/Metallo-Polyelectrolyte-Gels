423328ac3aa1bcb7a585ba02a8e56bad52a63d02
pw0908 <pw1017@ic.ac.uk>
2024-02-23 19:35:07 -0800

    Installation scripts for software

---
+++ Quantum-Chemistry/software/0-installation.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Author     : Pierre Walker (GitHub: @pw0908)
+# Date       : 2024-02-23
+# Description: Script to install the various packages required for the calculation
+
+# Install the various packages
+source ./1-orca.sh
+source ./2-molsimplify.sh
---
+++ Quantum-Chemistry/software/2-molsimplify.sh
@@ -0,0 +1,5 @@
+# Install molsimplify
+git clone https://github.com/hjkgrp/molSimplify.git
+cd molSimplify
+pip install -e .[dev]
+cd ..
