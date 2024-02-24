6d18876a1b2f8d22fe442a266ee7bf3f8b23fc9b
pw0908 <pw1017@ic.ac.uk>
2024-02-23 19:35:14 -0800

    Example input

---
+++ Quantum-Chemistry/submission/implicit_water_sodium.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Author     : Pierre Walker (GitHub: @pw0908)
+# Date       : 2024-02-23
+# Description: Script to set global variables and preferences for the calculation
+
+# Hardware ####################################################################
+
+export CPU_THREADS='20' # number of CPU threads to use (-1 = all available)
+export CPU_PINS='0'     # offset for CPU thread pinning (-1 = no offset)
+
+# System components ###########################################################
+# metal specification
+export METAL="Na"         # Give the metal name
+export OX_STATE="I"       # Give the oxidation state of the metal
+export METAL_CHARGE="1"   # Give the charge of the metal
+export SPIN="1"           # Give the spin of the metal complex (e.g. 5 for Fe(II), 4 for Fe(III))
+
+# ligand specification
+export LIGAND=("NC(C(=O)[O-])") # Give the ligand name
+export BINDING_SITES=("4,5")    # Give the binding sites for the ligand
+export LIGAND_CHARGE=("-1")     # Give the charge of the ligand
+export NUM_LIGAND=("3")         # Give the number of ligands in the complex
+
+# complex specification
+export COMPLEX_SPIN="1" # Give the spin of the complex
+
+# Calculation #################################################################
+# DFT specification
+export FUNCTIONAL="B3LYP"                       # Give the DFT functional
+export BASIS_SET="def2-TZVPP D3BJ CPCM(water)"  # Give the basis set
