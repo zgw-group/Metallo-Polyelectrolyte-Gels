398e490da108ce36ecc65fba020847abce15575a
pw0908 <pw1017@ic.ac.uk>
2024-02-27 18:52:59 -0800

    Initial input file design

---
+++ Molecular-Dynamics/submission/example.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Created by Pierre Walker (GitHub: @pw0908) on 2024-02-23
+
+#SBATCH --job-name=NaImpl
+#SBATCH --time=7-00:00:00
+
+# Slurm: Node configuration
+#SBATCH --partition=PersonalQ
+#SBATCH --nodes=1 --ntasks-per-node=1 --mem=4G
+#SBATCH --gres=gpu:0 --gpu-bind=closest
+
+# Slurm: Runtime I/O
+#SBATCH --output=/home/pjwalker/slurm-reports/slurm-%j.out --error=/home/pjwalker/slurm-reports/slurm_error-%j.out
+
+# built-in shell options
+# set -o errexit # exit when a command fails
+# set -o nounset # exit when script tries to use undeclared variables
+
+# simulation path variables
+proj_base_dir="$(pwd)/.."
+scripts_dir="${proj_base_dir}/scripts"
+params_dir="${proj_base_dir}/submission/input"
+
+input_globals=(
+    divalent_mpec_gel.sh
+)
+
+# start script
+date_time=$(date +"%Y-%m-%d %T")
+echo "START: ${date_time}"
+
+parallel --link --keep-order --ungroup --halt-on-error '2' --jobs '1' \
+    "${scripts_dir}/run.sh" "${params_dir}/{1}" -i \
+    ::: "${input_globals[@]}"
+
+# end script
+date_time=$(date +"%Y-%m-%d %T")
+echo "END: ${date_time}"
---
+++ Molecular-Dynamics/submission/input/divalent_mpec_gel.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Author     : Pierre Walker (GitHub: @pw0908)
+# Date       : 2024-02-23
+# Description: Script to set global variables and preferences for the MD Simulation
+
+# Hardware ####################################################################
+export CPU_THREADS='1'                     # number of CPU threads to use (-1 = all available)
+export CPU_PINS='1'                         # offset for CPU thread pinning (-1 = no offset)
+export GPUS='1'                             # number of GPUs to use (-1 = all available)
+export GPU_ID='0'                           # GPU ID to use (-1 = no GPU)
+
+# System components ###########################################################
+# metal specification
+export NMETAL="600"                         # Give the number of metal atoms
+export METAL_DIAMETER="0.5"                 # Give the radius of the metal
+export METAL_CHARGE="2"                     # Give the charge of the metal
+
+# polymer specification
+export NCHAIN="50"                          # Give the number of polymers
+export SPARSITY="41"                        # Give the charge sparsity of the polymer
+export NMONOMER="8"                         # Give the number of monomers in the polymer
+export BEAD_DIAMETER="1"                    # Give the radius of the polymer
+
+# co-ion specification
+export COION_DIAMETER="1.5"                 # Give the radius of the co-ion
+
+# Simulation #################################################################
+# Initialisation
+export DENSITY="0.05"                       # Give the initial density
+export TEMPERATURE="1"                      # Give the temperature
+export DIELECTRIC="0.15"                    # Give the dielectric constant
+
+# Production - NVE
+export TIME_STEP="0.005"                    # Give the time step
+export NUM_STEPS="35000000"                 # Give the number of steps
+
+# Production - Deformation
+export DEFORMATION="0.652e-2"               # Give the deformation rate
+export DEFORMATION_STEPS="300000"           # Give the number of steps
