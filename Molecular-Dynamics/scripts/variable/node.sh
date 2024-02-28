407d39becda4a7c5db2f2bb60dc3240aaf55c8dd
pw0908 <pw1017@ic.ac.uk>
2024-02-27 18:53:23 -0800

    variable specification

---
+++ Molecular-Dynamics/scripts/variable/node.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Author     : Pierre Walker (GitHub: @pw0908)
+# Date       : 2024-02-23
+# Description: Script to set generic global variables pertaining to the node
+#              hardware and software.
+# Notes      : Script assumes software has been installed in the software folder.
+
+# ##############################################################################
+# Set software and hardware ####################################################
+# ##############################################################################
+
+# set paths to executables
+script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+LAMMPS_BIN="${script_path}/../../software/lammps/lmp"
+export LAMMPS_BIN
+
+hostname=$(hostname -s)
+
+if [[ "${hostname}" == "pierre-walker" ]]; then
+    # If on Pierre's machine, get the list of CPUs you can pin to.
+    TOTAL_CPU=24
+    CPU_LIST="$(python3 ${script_path}/../parameters/cpu_avail.py ${TOTAL_CPU} $CPU_THREADS)"
+    export CPU_LIST
+    echo "CPUs pinned: ${CPU_LIST}"
+    PYTHON_BIN="/home/pjwalker/anaconda3/bin/python3"
+    export PYTHON_BIN
+fi    
---
+++ Molecular-Dynamics/scripts/variable/system.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Author     : Pierre Walker (GitHub: @pw0908)
+# Date       : 2024-02-23
+# Description: Script to set generic global variables pertaining to the system.
+# Notes      : Script should only be called from the main run.sh script.
+
+# find path to this script
+script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+project_path="${script_path}/../.."
+
+# ##############################################################################
+# Set simulation identifiers ###################################################
+# ##############################################################################
+
+# Polymer tag
+POLYMER_TAG="POL_${NCHAIN}-${SPARSITY}-${NMONOMER}"
+export POLYMER_TAG
+
+# Metal tag
+METAL_TAS="MET_${NMETAL}-${METAL_CHARGE}_${METAL_DIAMETER}"
+
+# system tag
+COMPLEX_TAG="diel_${DIELECTRIC}_dens_${DENSITY}_temp_${TEMPERATURE}"
+
+# Combine tags
+export TAG="${TAG_JOBID}-${METAL_TAG}-${LIGAND_TAG}-${COMPLEX_SPIN}-${METHOD_TAG}"
